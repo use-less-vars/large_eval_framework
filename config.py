eed59c5849d65437a2b3fdace1205a27988a8779
Jojo <johannesmatejcek@web.de>
2025-08-07 00:05:46 +0200

    Restructuring the project

--- config.py
+++
@@ -1,9 +0,0 @@
-import json
-from pathlib import Path
-
-def load_strategy_params(config_path: str) -> dict:
-    """Load strategy params"""
-    with open(Path(config_path)) as f:
-        all_params = json.load(f)
-        return all_params
-
